ec9475823de063e4d5413fd59391e98778512163
201601972-kimdongwook <62870938+201601972-kimdongwook@users.noreply.github.com>
2021-02-27 01:02:24 +0900

    initial commit - make loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './images/loading3.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className = "Loading">Loading</h1>
       </header>
     </div>
   );
