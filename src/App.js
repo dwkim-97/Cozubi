cb423fcb0b9b2e22b5f2e8488ad8f89633327ed3
201601972-kimdongwook <62870938+201601972-kimdongwook@users.noreply.github.com>
2021-02-27 01:55:59 +0900

    기본 페이지 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './images/loading3.png';
-import './App.css';
+import React from "react";
+import Home from "./components/Home"
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className = "Loading">Loading</h1>
-      </header>
-    </div>
+    <Home/>
   );
 }
 
