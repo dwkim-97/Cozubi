f827cc34a61d3a5f4479d90658dfba2fc5178fb4
201601972-kimdongwook <62870938+201601972-kimdongwook@users.noreply.github.com>
2021-03-01 00:44:29 +0900

    Twitter API failed / Set Grid to Coin List

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -10,20 +10,42 @@ class Home extends React.Component{
         isLoading: false,
     }
 
-    getTwitter = async () => {
-        const client = new Twitter({
-            TWITTER_API_KEY: "hoHg1DGawpy30gI9xXiQc2Whx",
-            TWITTER_API_SECRET: "OcxmfUJJDfdMHhOZyyxPaIjmUEzX1f7dXLzQOTnhh40mG337g1",
-            TWITTER_BEARER_TOKEN: "AAAAAAAAAAAAAAAAAAAAAGjRNAEAAAAAYKo3aCZ%2Fou2%2FDvJyoeSHnnioNQk%3Dkaq6IheHwGTQOSajTAOrxpx472niBInAkQGPJrDVv0EXjA67Hp",
-            TWIITER_ACCESS_TOKEN: "240048344-sfiRXc38T7PILeYyDYGmRelC3aUNbmC8G1P1zcjJ",
-            TWITTER_ACCESS_SECRET: "VDs8l9UKrlFVTTSv5OhX98mD49jyuTnBJ9uqHj0CA9mez",
-        })
-        client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
-            console.log(tweets);
-        });
 
-        this.setState({ isLoading: false })
+    getTwitter = async() => {
+        const needle = require('needle');
+        const token = "AAAAAAAAAAAAAAAAAAAAAGjRNAEAAAAAa3tvEgstb%2BkhSfeu1kyqzODKHmc%3DEPZetvdsJjzu2pbrid80GiDol1CSaqc6fAeSbOtMJFXEOMIyzo";
+        const endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
+        const params = {
+            'query': 'from:twitterdev -is:retweet',
+            'tweet.fields':'author-id',
+        }
+        const response = await needle('get', endpointUrl, params, {
+            headers: {
+                "User-Agent": "v2RecentSearchJS",
+                "authorization": `Bearer ${token}`,
+            }
+        })
+        if(response.body){
+            console.log(response.body);
+            return response.body;
+        }else{
+            throw new Error('Unsuccessful request');
+        }
     }
+    // getTwitter = async () => {
+    //     const axios = require('axios');
+    //     const client = new Twitter({
+    //         TWITTER_API_KEY: "hoHg1DGawpy30gI9xXiQc2Whx",
+    //         TWITTER_API_SECRET: "OcxmfUJJDfdMHhOZyyxPaIjmUEzX1f7dXLzQOTnhh40mG337g1",
+    //         TWITTER_BEARER_TOKEN: "AAAAAAAAAAAAAAAAAAAAAGjRNAEAAAAAa3tvEgstb%2BkhSfeu1kyqzODKHmc%3DEPZetvdsJjzu2pbrid80GiDol1CSaqc6fAeSbOtMJFXEOMIyzo",
+    //         TWIITER_ACCESS_TOKEN: "240048344-sfiRXc38T7PILeYyDYGmRelC3aUNbmC8G1P1zcjJ",
+    //         TWITTER_ACCESS_SECRET: "VDs8l9UKrlFVTTSv5OhX98mD49jyuTnBJ9uqHj0CA9mez",
+    //     })
+    //     const response = await axios.get('https://api.twitter.com/2/tweets/search/recent?query=from:NEM')
+    //     console.log(response);
+
+    //     this.setState({ isLoading: false })
+    // }
 
     getTime = () => {
         const date = new Date();
@@ -39,25 +61,25 @@ class Home extends React.Component{
     
 
     render() {
-        const { isLoading, movies } = this.state;
+        const { isLoading } = this.state;
         const {year, month, day} = this.getTime();
         const coins = ["ADA", "XEM", "XRP"]
-        //this.getTwitter();
+        this.getTwitter();
         return(
-            <div className="Loading">
+            <div>
                 {isLoading ?(
                     <header className="Loading-header">
-                        <img src= "images/loading3.png" className="Loading-img" alt="logo" />
-                        <h1 className = "Loading-text">Cozubi</h1>
+                        <img src= "images/loading4.png" className="Loading-img" alt="logo" />
+                        <h1 className = "Loading-text">COZUBI</h1>
                         <p>Loading..</p>
                     </header>
                 ) : (
                     <div>
                         <h1 className = "Show-Date">{year}/{month}/{day}</h1>
-                        <section className = "Coin-List">
+                        <section className = "Coin-List" >
                             {coins.map(coin => (
                                 <CoinBox
-                                    key = {coin} name ={coin}/>
+                                    calssName = "Coin-Box" key = {coin} name ={coin}/>
                             ))} 
                         </section>
                     </div>
