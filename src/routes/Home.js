1288db7442ca42e4647fe72260f6de844a629a69
201601972-kimdongwook <62870938+201601972-kimdongwook@users.noreply.github.com>
2021-02-28 11:58:41 +0900

    Twitter API keys saved.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -12,10 +12,11 @@ class Home extends React.Component{
 
     getTwitter = async () => {
         const client = new Twitter({
-            TWITTER_CONSUMER_KEY: "0ZquvNyZ4F88sVisX256LyHrm",
-            TWITTER_CONSUMER_SECRET: "g2o2Xtf8bjExpTbG2R2jwwwFBJRInErCZWWQdYGt1e2WjZJ0Cc",
-            TWIITER_ACCESS_TOKEN: "240048344-QE3wjuodlGo0s0AAC1gSTL3ZskDQrFEpinggAFnK",
-            TWITTER_ACCESS_SECRET: "62p2DAs3joe2InS8HpzBhbq1sv6Yi6c2upGjEHUTgSjTu",
+            TWITTER_API_KEY: "hoHg1DGawpy30gI9xXiQc2Whx",
+            TWITTER_API_SECRET: "OcxmfUJJDfdMHhOZyyxPaIjmUEzX1f7dXLzQOTnhh40mG337g1",
+            TWITTER_BEARER_TOKEN: "AAAAAAAAAAAAAAAAAAAAAGjRNAEAAAAAYKo3aCZ%2Fou2%2FDvJyoeSHnnioNQk%3Dkaq6IheHwGTQOSajTAOrxpx472niBInAkQGPJrDVv0EXjA67Hp",
+            TWIITER_ACCESS_TOKEN: "240048344-sfiRXc38T7PILeYyDYGmRelC3aUNbmC8G1P1zcjJ",
+            TWITTER_ACCESS_SECRET: "VDs8l9UKrlFVTTSv5OhX98mD49jyuTnBJ9uqHj0CA9mez",
         })
         client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
             console.log(tweets);
@@ -46,7 +47,7 @@ class Home extends React.Component{
             <div className="Loading">
                 {isLoading ?(
                     <header className="Loading-header">
-                        <img src= "/images/loading3.png" className="Loading-img" alt="logo" />
+                        <img src= "images/loading3.png" className="Loading-img" alt="logo" />
                         <h1 className = "Loading-text">Cozubi</h1>
                         <p>Loading..</p>
                     </header>
